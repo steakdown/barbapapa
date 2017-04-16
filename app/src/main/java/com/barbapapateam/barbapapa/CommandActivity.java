efa27b6ed1317146a60d3691ecc0ce5d6758c092
Timothée Tallon <timotheetallon@macprodetimotheetallon.adh.rezel.net>
2017-04-16 18:42:36 +0200

    Creating new activity : command

---
+++ app/src/main/java/com/barbapapateam/barbapapa/CommandActivity.java
@@ -0,0 +1,10 @@
+package com.barbapapateam.barbapapa;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by timotheetallon on 16/04/2017.
+ */
+
+public class CommandActivity extends AppCompatActivity {
+}

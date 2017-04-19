57b96a5b8f627cb7bfafe3ae83a9fb1c93f2caf1
Thomas Luo <thomas.luo@telecom-paristech.fr>
2017-04-19 13:05:32 +0200

    Guide fonctionnelle

--- app/src/main/java/com/barbapapateam/barbapapa/AdvancedRecommandationActivity.java
+++ app/src/main/java/com/barbapapateam/barbapapa/AdvancedRecommandationActivity.java
@@ -5,11 +5,13 @@ package com.barbapapateam.barbapapa;
  */
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.LinkedList;
@@ -29,25 +31,40 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
     //Zone de texte contenant la question
     TextView t1;
 
+    //Zone d'image contenant les images
+    ImageView v1;
+
     //On charge les bière dans une liste, et en fonction des réponses aux questions, on supprimera des éléments de la liste
     LinkedList<Beer> beers;
 
+    public AdvancedRecommandationActivity() {
+        // Required empty public constructor
+    }
+
+
 
     //On distinguera les questions en questions principales (question1, 2 et 3) et questions secondaires (contenue des question1, 2 et 3)
-    String[] question1 = {"Voulez vous une bière en Pression ?", "Donc plutôt en bouteille ?"};
-    String[] question2 = {"Voulez vous une bière Blonde ?", "Plutôt blanche donc ?" ,"Plutôt ambrée du coup ? ", "Non plus ? Plutôt brune alors ?"};
-    String[] question3 = {"Voulez vous une bière forte ?", "On part sur une bière plutôt douce donc ?"};
+    private String[] question1 = {"Voulez vous une bière en Pression ?", "Donc plutôt en bouteille ?"};
+    private String[] question2 = {"Voulez vous une bière Blonde ?", "Plutôt blanche donc ?" ,"Plutôt ambrée du coup ? ", "Non plus ? Plutôt brune alors ?"};
+    private String[] question3 = {"Voulez vous une bière forte ?", "On part sur une bière plutôt douce donc ?"};
 
     private String[][] questions = {question1, question2, question3};
 
     private boolean[] bottle = {true, false};
     private String[] color = {"Blonde", "Blanche", "Ambrée", "Brune"};
     private String[] degre = {"forte", "douce"};
-
     private String[][] attributes = {{},color, degre};
+
+    private int[] image1 = {R.drawable.pression,R.drawable.bouteille};
+    private int[] image2 = {R.drawable.blonde,R.drawable.blanch,R.drawable.ambree,R.drawable.brune};
+    private int[] image3 = {R.drawable.forte, R.drawable.douce};
+    private int[][] images = {image1,image2,image3};
+
+
     private int indice1 = 0 ;
     private int indice2 = 0 ;
     String question = questions[0][0];
+    int image = images[0][0];
 
 
     //Affiche le résultat de la recherche
@@ -57,31 +74,38 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
     }
 
     //Fonction proposant la bière la mieux notée correspondant aux goûts de l'utilisateur
-    public Beer getMyBeer(){
-        LinkedList<Beer> listResult = getResult();
+    public Beer getMyBeer(LinkedList<Beer> listResult ){
         Beer myBeer = listResult.getFirst();
-        if(myBeer == null){
-            //Afficher un message comme quoi il n'y a aucune réponse
-        } else {
+        if(listResult.size() != 0){
             for (int i = 1; i< listResult.size(); i++){
                 if(listResult.get(i).getNote()> myBeer.getNote()){
                     myBeer = listResult.get(i);
                 }
             }
+        } else {
+            t1.setText("Aucun résultat");
         }
         return myBeer;
-
     }
 
     //Permet de passer à la question principale suivante
     private void getNextQuestion(){
         indice1++;
         if(indice1 == 3){
-            getResult();
+            t1.setText("ok");
+            /*
+            LinkedList<Beer> listResult = getResult();
+            if(listResult.size() != 0) {
+                LaunchCommandActivity(getMyBeer(listResult));
+            } else {
+                t1.setText("Aucun résultat");
+            }*/
         } else {
             indice2 = 0;
             question = questions[indice1][indice2];
             t1.setText(question);
+            image = images[indice1][indice2];
+            v1.setImageResource(image);
         }
     }
 
@@ -94,6 +118,8 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
             indice2 = 0;
             question = questions[indice1][indice2];
             t1.setText(question);
+            image = images[indice1][indice2];
+            v1.setImageResource(image);
         }
     }
 
@@ -104,25 +130,32 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
             if (indice2 == 2) {
                 indice2 = 0;
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             } else {
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             }
         } else if (indice1 == 1) {
             if (indice2 == 4) {
                 indice2 = 0;
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             } else {
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             }
         } else {
             if(indice2 == 2 ){
                 indice2 = 0;
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             } else {
                 question = questions[indice1][indice2];
+                image = images[indice1][indice2];
             }
         }
         t1.setText(question);
+        v1.setImageResource(image);
     }
 
     // L'utilisateur répond oui
@@ -130,17 +163,20 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
     private void yes(){
         //On supprime des éléments de la liste.
         if (indice1 == 2){
-            for(int i = 0; i< beers.size(); i++){
-                Beer beer = beers.get(i);
-                //L'utilisateur veut une bière forte
-                //Dans le cas du degré, on doit faire un prétraite car ABV est un float
-                if(indice2 == 0)
-                    if(beer.ABV <= 6)
+            if(indice2 == 0){
+                for(int i = 0; i< beers.size(); i++){
+                    Beer beer = beers.get(i);
+                    if(beer.ABV < 6){
                         beers.remove(i);
-                        //L'utilisateur veut une bière douce.
-                    else
-                    if(beer.ABV >6)
+                    }
+                }
+            } else if (indice2 == 1){
+                for(int i = 0; i< beers.size(); i++){
+                    Beer beer = beers.get(i);
+                    if(beer.ABV >= 6){
                         beers.remove(i);
+                    }
+                }
             }
         } else if (indice1 == 0) {
             //pour les autres question, les listes dans attributes correspondent a la valeur des attributs de Beer.
@@ -153,7 +189,7 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
         } else if (indice1 == 1) { //pour les autres question, les listes dans attributes correspondent a la valeur des attributs de Beer.
             for (int i = 0; i < beers.size(); i++) {
                 Beer beer = beers.get(i);
-                if (beer.getColor().equals(attributes[indice1][indice2])){
+                if (!beer.getColor().equals(attributes[indice1][indice2])){
                     beers.remove(i);
                 }
             }
@@ -166,6 +202,7 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
     private void no(){
         getNextNuance();
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -181,6 +218,8 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
 
         //Zone de texte contenant la question
         t1 = (TextView) findViewById(R.id.ARtextView);
+
+        v1 = (ImageView) findViewById(R.id.ARImageView);
 
 
         //connection des boutons entre model et view
@@ -232,5 +271,12 @@ public class AdvancedRecommandationActivity extends Activity implements View.OnC
 
         return super.onOptionsItemSelected(item);
     }
+/*
+    private void LaunchCommandActivity(Beer beer) {
 
+        //Intent commandIntent = new Intent(this.getContext(), CommandActivity.class);
+      //  Database.setBeerForCommand(beer);
+     //   startActivity(commandIntent);
+
+    }*/
 }

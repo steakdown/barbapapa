fcec37a42af87aa2382b46f3aa3a178ef2145056
David Jourdan <dav.jourdan@gmail.com>
2017-04-23 20:14:54 +0200

    change recomm layout

--- app/src/main/java/com/barbapapateam/barbapapa/BasicRecomFragment.java
+++ app/src/main/java/com/barbapapateam/barbapapa/BasicRecomFragment.java
@@ -29,7 +29,7 @@ public class BasicRecomFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_recomm, container, false);
         ListView beerList = (ListView) view.findViewById(R.id.beer_list_view);
 
         LinkedList<Beer> beers = (LinkedList<Beer>) Database.beers.clone();

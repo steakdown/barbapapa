efb69587068a3ddc991d3080013f8e722544b5aa
Paul Vinchon <ipvron@yahoo.fr>
2017-05-29 01:42:47 +0200

    Déplacement vers le dossier drawablen rajout des 5 bières restantes, json+java

--- app/src/main/java/com/barbapapateam/barbapapa/Database.java
+++ app/src/main/java/com/barbapapateam/barbapapa/Database.java
@@ -22,6 +22,7 @@ public class Database {
     static void initDb(Context context)
     {
         imageIDs = new Hashtable<String, Integer>();
+
         imageIDs.put("Kwak", R.drawable.kwak);
         imageIDs.put("Delirium", R.drawable.delirium);
         imageIDs.put("Tripel Karmeliet", R.drawable.karmeliet);
@@ -36,9 +37,41 @@ public class Database {
         imageIDs.put("Kronenbourg", R.drawable.kronenbourg);
         imageIDs.put("Grimbergen Blanche", R.drawable.blanche);
         imageIDs.put("Grimbergen Double Ambrée", R.drawable.grim_ambree);
+        imageIDs.put("Superbock", R.drawable.superbock);
+        imageIDs.put("San Miguel", R.drawable.sanmiguel);
+        imageIDs.put("Asahi", R.drawable.asahi);
+        imageIDs.put("Brahma", R.drawable.brahma);
+        imageIDs.put("Pilsen Urquell", R.drawable.pilsner_urquell);
+        imageIDs.put("Hoegaarden", R.drawable.hoegaarden);
+        imageIDs.put("Angelus", R.drawable.angelus);
+        imageIDs.put("Duchesse Anne", R.drawable.duchesse_anne);
+        imageIDs.put("Goudale", R.drawable.goudale);
+        imageIDs.put("Queue de charrue", R.drawable.queue_de_charrue);
+        imageIDs.put("Pietra", R.drawable.pietra);
+        imageIDs.put("Bush triple", R.drawable.bush_triple_ambree);
+        imageIDs.put("Orval", R.drawable.orval);
+        imageIDs.put("Brooklyn East IPA", R.drawable.brooklyn_east);
+        imageIDs.put("Big Job", R.drawable.big_job);
+        imageIDs.put("Carolus", R.drawable.carolus);
+        imageIDs.put("Duvel", R.drawable.triple_hop_duvel);
+        imageIDs.put("Fruit défendu", R.drawable.fruit_defendu);
+        imageIDs.put("Gauloise brune", R.drawable.gauloise_brune);
+        imageIDs.put("Mort subite", R.drawable.mort_subite);
+        imageIDs.put("Queue de charrue rouge", R.drawable.queue_rouge);
+        imageIDs.put("Kasteel rouge", R.drawable.kasteel_rouge);
+        imageIDs.put("BrewDog 5AM saint", R.drawable.fiveam_saint);
+        imageIDs.put("Faro", R.drawable.faro);
+        imageIDs.put("Gueuze", R.drawable.gueuze);
+        imageIDs.put("BrewDog Jet Black Heart", R.drawable.jet_black_heart);
+        imageIDs.put("Guinness", R.drawable.guinness);
+        imageIDs.put("Erdinger", R.drawable.erdinger);
+        imageIDs.put("Page 24", R.drawable.page_24);
+        imageIDs.put("Vezelay Stout", R.drawable.vezelay);
+        imageIDs.put("Kasteel Donker", R.drawable.kasteel_donker);
 
-        beers = Utils.getBeersFromJSON("beers.json", context);
         commands = new LinkedList<>();
+        beers = Utils.getBeersFromJSON("beers.json", context);
+
 
         initialized = true;
     }
@@ -51,7 +84,8 @@ public class Database {
 
     static int getImageIdFromName(String name)
     {
-        return imageIDs.get(name);
+        int id = imageIDs.get(name);
+        return id;
     }
 
     public static void setBeerForCommand(Beer beer)
